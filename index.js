7cb27dfb998cd9c7f62c51d0f92d10e1eb9ae135
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-07-20 15:02:08 +0530

    add handler for routes

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const serverLogic = function(req, res){
   const parsedUrl = url.parse(req.url, true);
   // Getting trimmed url
   const trimmedUrl = parsedUrl.pathname.replace(/^\/+|\/+$/g,"");
+  console.log(trimmedUrl);
   // Getting request method
   const method = req.method;
   // Getting query
@@ -36,12 +37,43 @@ const serverLogic = function(req, res){
   req.on("end", function(){
     buffer+=decoder.end();
 
+    // Getting the handler for the route
+    var selectedHandler = typeof(routes[trimmedUrl]) == "function"?routes[trimmedUrl]: handler.notFound;
+    console.log(typeof(routes[trimmedUrl]));
+
+    // Preparing data object
+    const data = {
+      trimmedUrl: trimmedUrl,
+      method: method,
+      query: query,
+      headers: headers,
+      payload: buffer
+    };
+
+    // Calling selectedHandler
+    selectedHandler(data, function(statusCode, payload){
+      // Setting default value for status code
+      statusCode = typeof(statusCode) == "number"?statusCode:200;
+      // Setting default value for payload
+      payload = typeof(payload) == "object"?payload:{};
+      // converting payload to string
+      var payloadString = JSON.stringify(payload);
+
+      // Sending res back
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Logging the response
+      console.log("response: ", statusCode, payload);
+    });
+
   });
 };
 
 // Set port for HTTP server to listen to
 httpServer.listen(80, function(){
-  console.log("server is listening to port 80, press ctrl+c to exits");
+  console.log("server is listening to port 80, press ctrl+c to exit");
 });
 
 // Initializing container for handlers
